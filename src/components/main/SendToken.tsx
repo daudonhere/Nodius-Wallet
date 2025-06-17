7b6cde38fd96e764d41b7c47bca8b062b5e29108
daudonhere <hellodadedaud@gmail.com>
2025-06-17 09:02:09 +0700

    feat: add transfer & history action

--- src/components/main/SendToken.tsx
+++ src/components/main/SendToken.tsx
@@ -1,15 +1,11 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { Send } from "lucide-react";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
+import { Send, ScanLine, Star } from "lucide-react";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export function ReceiveToken() {}
 
 export function SendToken() {
   return (
@@ -21,26 +17,57 @@ export function SendToken() {
             </CardContent>
         </Card>
       </DrawerTrigger>
-      <DrawerContent className="h-full">
-          <div className="mx-auto h-full w-full max-w-sm">
-          <DrawerHeader>
-              <DrawerTitle>Move Goal</DrawerTitle>
-              <DrawerDescription>Set your daily activity goal.</DrawerDescription>
-          </DrawerHeader>
-            <div className="p-4 pb-0">
-              <div className="flex items-center justify-center space-x-2">
-                  <span className="sr-only">Decrease</span>
-              <div className="flex-1 text-center">
-                  <div className="text-muted-foreground text-[0.70rem] uppercase">
-                  Calories/day
-                  </div>
-              </div>
+      <DrawerContent className="flex flex-col w-full h-full">
+          <div className="flex flex-row gap-2 px-4 py-8 w-full h-[15%]">
+            <div className="flex w-[75%] h-full">
+              <Input type="email" placeholder="Wallet Address" />
+            </div>
+            <div className="flex w-[25%] h-full">
+              <Button variant="outline">
+                Send <Send />
+              </Button>
             </div>
           </div>
-          <DrawerFooter>
-              <DrawerClose asChild>
-              </DrawerClose>
-          </DrawerFooter>
+          <div className="flex flex-row gap-2 px-4 w-full h-[10%]">
+            <div className="flex w-[75%] h-full">
+              <h2 className="flex font-bold text-lg mt-2 ml-1">
+                Favorite Transaction
+              </h2>
+            </div>
+            <div className="flex w-[25%] h-full">
+              <Button variant="outline">
+                Scan <ScanLine />
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 py-2 px-4 w-full h-[75%] overflow-y-scroll no-scrollbar">
+            <Card className="flex w-full h-16 justify-center">
+              <CardContent className="flex flex-row flex-1 gap-1 py-1 px-4">
+                <div className="flex flex-row flex-1 items-center gap-2">
+                  <div className="flex justify-start items-center">
+                    <Avatar className="w-11 h-11 border-2">
+                       <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>RR</AvatarFallback>
+                    </Avatar>
+                  </div>
+                  <div className="flex flex-1 flex-col justify-start gap-1">
+                    <div className="flex flex-1 justify-start">
+                      <h2 className="flex font-bold text-sm">
+                        Goki Kato
+                      </h2>
+                    </div>
+                    <div className="flex flex-1 justify-start">
+                      <h6 className="flex font-semibold text-xs">
+                        0X87824783..234212
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-1 items-center justify-end">
+                  <Star />
+                </div>
+              </CardContent>
+            </Card>
           </div>
       </DrawerContent>
     </Drawer>
